aeaf9efee9090f258f51acedcdb150c9e49d05ce
sid773 <89004995+sid773@users.noreply.github.com>
2021-08-16 12:53:42 +0530

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+var temperature;
+
+//TODO GO TO GOOGLE AND GET THE DATA
+temperature=10;
+
+
+if (temperature<0){
+    console.log("its very hot");
+}else{
+    if (temperature<0){
+        console.log("its moderate");
+    }else{
+        console.log("its cool");
+    }
+    
+}
